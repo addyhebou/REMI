abdc65e003cbc3fe8f136f0414706092a69f1ec4
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2021-01-30 23:43:53 -0500

    feat(tasks): now all tasks are displaying

--- app/src/Components/GoalSet.js
+++ app/src/Components/GoalSet.js
@@ -6,36 +6,34 @@ export default class GoalSet extends Component {
         super(props)
     
         this.state = {
-             title: this.props.title,
-             backgroundColor: this.props.color,
-             tasks: [],
+            title: this.props.title,
+            backgroundColor: this.props.color,
+            tasks: [],
         };
     };
 
     updateTasks = () => {
         let i = 0;
+        const temp = [];
         for (i = 0; i < this.props.children.length; ++i){
-            this.setState({tasks: this.state.tasks.push(
-                <h1>{this.props.children[i].props.children}</h1>
-            )});
-            console.log(this.state.tasks[i].props.children);
+            temp.push(this.props.children[i].props.children);
         }
+        this.setState(
+            {tasks: temp}
+        )
     }
 
-
-
-
+    componentDidMount = () =>{
+        this.updateTasks()
+    }
 
     render() {
         return (
             <div>
-                <h2 onClick = {this.updateTasks} className = "Title" style = {{backgroundColor: "#"+this.state.backgroundColor}}>{this.state.title}</h2>
-                {/* <div className = "Task">{this.props.children}</div> */}
-                {/* <div className = "Task">{this.state.tasks}</div> */}
+                <h2 className = "Title" style = {{backgroundColor: "#"+this.state.backgroundColor}}>{this.state.title}</h2>
                 <ul>
                     {this.state.tasks.map((task) => {
-                        console.log(task)
-                        return <li>{task}</li>
+                        return <div className = "Task">{task}</div>
                     })}
                 </ul>
             </div>
