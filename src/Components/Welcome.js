659c1e6b37552b27c538ed2b4368c18e276c5e0d
addyhebou <addyhebou@gmail.com>
2022-04-05 23:52:53 -0400

    fix(package.json): updated homepage

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -5,9 +5,55 @@ import ToDoPreview from './ToDoPreview';
 const lib = require('../Services/getWeather');
 
 export default function WelcomeFunc() {
+  const getWeather = async () => {
+    try {
+      // Get the city and region
+      let res = await fetch(`http://ip-api.com/json/`);
+      if (!res.ok) throw new Error('HTTP error - ip-api');
+      let json = await res.json();
+      const city = json['city'];
+      const region = json['region'];
+
+      // API key for WeatherAPI
+      const appID = 'f33518e9773745fdafd61453220701';
+
+      // Get the temp, condition, icon, feel
+      res = await fetch(
+        `http://api.weatherapi.com/v1/forecast.json?key=${appID}&q=${city}&aqi=no`
+      );
+      if (!res.ok) throw new Error('HTTP error – weatherapi');
+      json = await res.json();
+      const temp = json['current']['temp_f'];
+      const condition = json['current']['condition']['text'].toLowerCase();
+      const icon = `http://${json['current']['condition']['icon'].slice(2)}`;
+      const feel = json['current']['feelslike_f'];
+
+      // Get the day
+      const date = new Date();
+      const hour = date.getHours();
+      const day =
+        hour >= 12 ? (hour >= 17 ? 'evening' : 'afternoon') : 'morning';
+
+      const message =
+        temp > 50
+          ? temp > 63
+            ? temp > 80
+              ? "It's burning so dress like its summer time."
+              : 'Long sleeve shirts, pants, and sneaks is the way to go.'
+            : 'Dress for chilly weather.'
+          : 'Layer up a lot, man.';
+
+      // Return all 7 properties
+      return [city, region, temp, condition, icon, feel, day, message];
+    } catch (e) {
+      console.log('we made it here...');
+      console.log(e);
+      return;
+    }
+  };
   useEffect(async () => {
     try {
-      const vals = await lib.getWeather();
+      const vals = await getWeather();
       setCity(vals[0]);
       setRegion(vals[1]);
       setTemp(vals[2]);
@@ -17,6 +63,7 @@ export default function WelcomeFunc() {
       setTimeOfDay(vals[6]);
       setMessage(vals[7]);
     } catch (e) {
+      console.log('We reached an error on reading the weather...');
       console.log(e);
     }
   }, []);
@@ -31,7 +78,7 @@ export default function WelcomeFunc() {
   const [message, setMessage] = useState('');
 
   return (
-    <div className="welcome">
+    <div className='welcome'>
       <h1>Good {timeOfDay}, Addy.</h1>
       {temp ? (
         <div>
